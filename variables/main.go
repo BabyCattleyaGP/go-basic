325e82626911cd61b4837f1cfe7826c0cf822633
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2020-04-15 14:02:44 +0700

    implement concat string

--- variables/main.go
+++ variables/main.go
@@ -10,6 +10,15 @@ func main(){
 	
 	var floatt float32 = 5.8
 
+	var tryString string = "Baso"
+
+	days := 6
+	var hours = 24 
+
+	//all string use +, otherwise can use ,
+	fmt.Println("Day spent in hours", days * hours)
+
+	fmt.Println("My favorite snack is " + tryString)
 	fmt.Println("Print number from var:", number)
 	fmt.Println("Print float number from var:", floatt)
     fmt.Println("The earth gravity is", earthsGravity)
