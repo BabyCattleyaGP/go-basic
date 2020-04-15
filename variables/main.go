3a1f7888613fe3955a7cb0b83d3d4dfe1f61b05e
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2020-04-15 13:33:56 +0700

    try create variable

---
+++ variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	const earthsGravity = 9.80665
+	
+	var number int8
+	number = 30
+	
+	var floatt float32 = 5.8
+
+	fmt.Println("Print number from var:", number)
+	fmt.Println("Print float number from var:", floatt)
+    fmt.Println("The earth gravity is", earthsGravity)
+	fmt.Println("23 multiplies by 4 is", 23 * 4)
+}
