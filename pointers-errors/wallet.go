56fed4e595c9c7da4264a8abc791d6e8c3f819d1
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2020-05-27 15:37:08 +0700

    Green: test passed with pointers

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,14 +1,20 @@
 package main
 
+import "fmt"
+
 // Wallet struct
-type Wallet struct{}
+type Wallet struct {
+	// lowercase symbol: private outside the package it's defined in
+	balance int
+}
 
 // Deposit function to know amount user deposit
-func (w Wallet) Deposit(amount int) {
-
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
 }
 
 // Balance function to know amount of user balance
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Balance() int {
+	return w.balance
 }
