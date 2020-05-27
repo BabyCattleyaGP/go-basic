43ded9efcd8ee388bb3e530416a18a60486827e4
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2020-05-27 15:44:24 +0700

    Green: Refactor using Bitcoin datatype

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -2,19 +2,31 @@ package main
 
 import "fmt"
 
+// Stringer is implemented by any value that has a String method
+type Stringer interface {
+	String() string
+}
+
+// Bitcoin type (type MyName OriginalType)
+type Bitcoin int
+
+// String method for bitcoin
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // Wallet struct
 type Wallet struct {
 	// lowercase symbol: private outside the package it's defined in
-	balance int
+	balance Bitcoin
 }
 
 // Deposit function to know amount user deposit
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance function to know amount of user balance
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
