09d78d545521cf2e104bc4eb96349845c0acd294
BabyCattleyaGP <baby16001@mail.unpad.ac.id>
2020-05-27 15:30:39 +0700

    Red: add minimum function to failed the test

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -1,5 +1,14 @@
 package main
 
 // Wallet struct
-type Wallet struct {
+type Wallet struct{}
+
+// Deposit function to know amount user deposit
+func (w Wallet) Deposit(amount int) {
+
+}
+
+// Balance function to know amount of user balance
+func (w Wallet) Balance() int {
+	return 0
 }
